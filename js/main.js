f3dd00efc100a88825f750f545bb08fbf02b3b20
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-02-23 17:41:22 +0100

    esercizio base completato

--- js/main.js
+++ js/main.js
@@ -5,21 +5,28 @@ for (let i=1; i<101; i++) {
     if(i % 15 === 0){
         console.log('FizzBuzz');
         elementoLista.innerHTML=("FizzBuzz");
+        elementoLista.classList.add(`classFizzBuzz`);
+        elementoLista.classList.add(`class-${i}`);
         ul.append(elementoLista);
     }
     else if (i % 3 === 0){
         console.log('Buzz');
         elementoLista.innerHTML=("Buzz");
+        elementoLista.classList.add(`classBuzz`);
+        elementoLista.classList.add(`class${i}`);
         ul.append(elementoLista);
     }
     else if(i % 5 === 0){
         console.log('Fizz');
         elementoLista.innerHTML=("Fizz");
+        elementoLista.classList.add(`classFizz`);
+        elementoLista.classList.add(`class${i}`);
         ul.append(elementoLista);
     }
     else{
         console.log(i);
         elementoLista.innerHTML=(i);
+        elementoLista.classList.add(`class${i}`);
         ul.append(elementoLista);
     }
 }   
